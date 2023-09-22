ab241c5883e54f6e5a5f3cd95b7a8a48bca830af
jc <wnwlcks123@gmail.com>
2023-09-22 21:43:39 +0900

    Todo List Toggle 및 삭제 기능 생성하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import { useRecoilValue } from 'recoil';
 import './App.css';
+import TodoItem from './components/TodoItem';
 import TodoItemCreator from './components/TodoItemCreator';
+import { todoListState } from './todoAtoms';
 
 function App() {
+  const todoList = useRecoilValue(todoListState);
+  console.log(todoList)
   return (
     <div className="App">
       <TodoItemCreator />
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
     </div>
   );
 }
