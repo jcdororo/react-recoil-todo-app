de738fe16ebfd4f781909941fad2113b9990eb28
jc <wnwlcks123@gmail.com>
2023-09-22 21:09:47 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoItemCreator from './components/TodoItemCreator';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoItemCreator />
+    </div>
+  );
+}
+
+export default App;
